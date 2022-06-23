bdb4484cc77c0e1fc30d69782ac94e8eeb1b7233
Segun Adekolu <segunadekolu@gmail.com>
2022-06-23 17:46:45 +0100

    calculator task

---
+++ calc.js
@@ -0,0 +1,27 @@
+function getCalc(){
+
+    let result;
+    let operator= prompt("Enter an operator: + - * /");
+
+    let num1= parseFloat(prompt("Enter a number:"));
+    let num2 = parseFloat(prompt("Enter the second number:"));
+
+    if(operator == "+"){
+        result= num1 + num2
+    }
+
+    else if(operator == "-"){
+        result= num1 - num2
+    }
+
+    else if(operator == "/"){
+        result= num1 / num2
+    }
+
+    else if(operator == "*"  || operator == "&divide;"){
+        result= num1 * num2
+    }
+
+    alert("here is your answer: " + result)
+}
+
